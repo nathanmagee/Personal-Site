56baadf09df03f7b6aa84e585b88c95045713c1c
mageen <93553480+nathanmagee@users.noreply.github.com>
2021-11-14 08:02:23 +0000

    +Started adding lots of comments *Changed person.jpg

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 wget https://github.com/sass/dart-sass/releases/download/1.32.8/dart-sass-1.32.8-linux-ia32.tar.gz
-
 tar xvzf dart-sass-1.32.8-linux-ia32.tar.gz
 
 export PATH="./dart-sass:$PATH"
 sass --watch css/main.scss css/main.css
-
-
 
 # then type export PATH="./dart-sass:$PATH" or use cd dart-sass
 # now you can use SASS!
